ca617365dd93f6a7e841d8284d607e2e87ea15b5
Abby Lorber <abberts07@gmail.com>
2020-10-17 21:15:27 +0000

    finished exercise 1.9.4: loading money to card

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,12 +1,15 @@
 public class CardRunner {
 	public static void main(String args[]){
-		TerrapinCard card = new TerrapinCard (5);
+		TerrapinCard card = new TerrapinCard (10);
 		System.out.println(card);
 
-		card.payGourmet();
+		card.loadMoney(15);
 		System.out.println(card);
 
-		card.payGourmet();
+		card.loadMoney(10);
+		System.out.println(card);
+
+		card.loadMoney(200);
 		System.out.println(card);
 	}
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -6,11 +6,18 @@ public class TerrapinCard {
 			this.balance = balance; 
 		}
 
+		public void loadMoney(double amount) {
+			if ((amount + this.balance) <= 150.0) {
+				balance = this.balance + amount;
+			} else {
+				balance =  150.0;
+			}
+		}
+
 		public void payEconomical() {
 			if (balance >= 2.4){
 			  balance = this.balance - 2.4;
 			}
-
 		}
 
 		public void payGourmet() {
