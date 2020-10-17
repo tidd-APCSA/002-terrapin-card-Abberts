79abfe25825fbfe43649c0228ba7f7e8c30dda93
Abby Lorber <abberts07@gmail.com>
2020-10-17 05:39:43 +0000

    testing to see what this commit & push thing does.

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,19 @@
 public class CardRunner {
-    public static void main(String args[]){
-        // ☕
-        System.out.println("hey");
+	public static void main(String args[]){
+		TerrapinCard card = new TerrapinCard(50);
+		System.out.println(card);
 
-    }
+		card.payEconomical();
+		System.out.println(card);
+
+		card.payGourmet();
+		card.payEconomical();
+		System.out.println(card);
+		card.payEconomical();
+		System.out.println(card);
+
+		card.payGourmet();
+		card.payEconomical();
+		System.out.println(card);
+	}
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,21 @@
 public class TerrapinCard {
 
+	private double balance;
+
+		public TerrapinCard(double balance) {
+			this.balance = balance; 
+		}
+
+		public void payEconomical() {
+			balance - 2.50;
+		}
+
+		public void payGourmet(double balance) {
+			return balance - 4.0;
+		}
+
+		public String toString() {
+			return "This card has " + this.balance + " dollars.";
+		}
 
 }
